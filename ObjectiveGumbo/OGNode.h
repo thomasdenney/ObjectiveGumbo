8a5a1bb4c6c93dd8501a61f9d910dd612e107766
Programming Thomas <programmingthomas@gmail.com>
2014-10-16 15:59:29 +0100

    @import Foundation

--- ObjectiveGumbo/OGNode.h
+++ ObjectiveGumbo/OGNode.h
@@ -14,7 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import <Foundation/Foundation.h>
+@import Foundation;
+
 #import "OGUtility.h"
 #import "NSString+OG.h"
 
--- ObjectiveGumbo/ObjectiveGumbo.h
+++ ObjectiveGumbo/ObjectiveGumbo.h
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import <Foundation/Foundation.h>
+@import Foundation;
 
 #import "OGElement.h"
 #import "OGNode.h"
