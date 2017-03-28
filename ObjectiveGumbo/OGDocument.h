9feb5791170777ea81dc8afa4c34a4231d6b4217
Richard Warrender <richard@vividreflection.com>
2017-03-28 12:15:24 +0100

    Added some documentation to OGDocument node properties and enums.

--- ObjectiveGumbo/OGDocument.h
+++ ObjectiveGumbo/OGDocument.h
@@ -8,21 +8,46 @@
 #import "OGNode.h"
 #import "OGElement.h"
 
+/**
+ * Indicates quirks mode that should be triggered based on provided doctype
+ * See https://en.wikipedia.org/wiki/Quirks_mode for more info
+ */
 typedef NS_ENUM(NSInteger, OGDocumentDocType) {
-    OGDocumentDocTypeNoQuirks,
-    OGDocumentDocTypeQuirks,
-    OGDocumentDocTypeLimitedQuirks
+    OGDocumentDocTypeNoQuirks,          /* Document is being read using strict mode */
+    OGDocumentDocTypeQuirks,            /* Document is using full quirks mode */
+    OGDocumentDocTypeLimitedQuirks      /* Document is being read using almost mode */
 };
 
+/**
+ * Converts OGDocType enum to an NSString for use in logging to the console
+ */
 NSString* NSStringFromOGDocType(OGDocumentDocType quirksMode);
 
 @interface OGDocument : OGElement
 
+/**
+ * Indicates document has provided a doc type and it isn't automatically inferred.
+ */
 @property (nonatomic, assign, readonly) BOOL hasDocType;
+
+/**
+ * Shortcut for seeing if quirks mode should be enabled
+ */
 @property (nonatomic, assign, readonly) BOOL quirksMode;
 
+/**
+ * The doctype name. This will usually be html
+ */
 @property (nonatomic, copy, readonly) NSString *name;
+
+/**
+ * Doctype Public identifier if one is specified
+ */
 @property (nonatomic, copy, readonly) NSString *publicIdentifier;
+
+/**
+ * Doctype Public identifier if one is specified
+ */
 @property (nonatomic, copy, readonly) NSString *systemIdentifier;
 
 @end
